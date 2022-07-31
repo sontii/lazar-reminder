a0b0753115eccb83b8576a07a0f81db9db00f8ee
sontii <sontii78@gmail.com>
2022-07-31 16:13:59 +0200

    pandas

--- sendmail.py
+++ sendmail.py
@@ -1,29 +1,32 @@
 import smtplib
-import csv
-import io
+import pandas as pd
+from io import StringIO
+
+
+def main():
+   df = pd.read_excel('emlekezteto.xlsx', skiprows=4)
+   print(df)
+
+
+def sendMail():
+   sender = 'ertesito@lazarteam.hu'
+   receivers = ['f.ferenc@lazarteam.hu']
+
+   message = """From: Havi ertesito <ertesito@lazarteam.hu>
+   To: To konyveles <f.ferenc@lazarteam.hu>
+   Subject: Havi emlekeztete email
+
+   This is a test e-mail message.
+   """
+
+   try:
+      smtpObj = smtplib.SMTP('localhost')
+      smtpObj.sendmail(sender, receivers, message)
+      print("Successfully sent email")
+   except smtplib.SMTPException:
+      print("Error: unable to send email")
 
 
 
-with io.open('emlekezteto.csv', encoding='utf-8') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print(row)
-
-
-
-sender = 'ertesito@lazarteam.hu'
-receivers = ['f.ferenc@lazarteam.hu']
-
-message = """From: Havi ertesito <ertesito@lazarteam.hu>
-To: To konyveles <f.ferenc@lazarteam.hu>
-Subject: Havi emlekeztete email
-
-This is a test e-mail message.
-"""
-
-try:
-   smtpObj = smtplib.SMTP('localhost')
-   smtpObj.sendmail(sender, receivers, message)
-   print("Successfully sent email")
-except smtplib.SMTPException:
-   print("Error: unable to send email")
+if __name__ == "__main__":
+    main()
