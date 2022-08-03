10d7e84d6fb69d66abd6e3ef3c1a17e75033fe28
Fridrich Ferenc <sontii78@gmail.com>
2022-08-03 20:47:26 +0200

    errorMail, and logging

--- sendmail.py
+++ sendmail.py
@@ -1,30 +1,60 @@
 import smtplib
+from email.mime.text import MIMEText
 import pandas as pd
-from io import StringIO
+from datetime import datetime, timedelta
+import logging
+
+
+logging.basicConfig(filename="logfile.log", encoding='utf-8', level=logging.INFO)
 
 
 def main():
-   df = pd.read_excel('emlekezteto.xlsx', skiprows=4)
-   print(df)
+   try:
+      df = pd.read_excel('//192.168.103.101/common/Doks/emlekezteto/emlekezteto.xlsx', skiprows=3)
+   except Exception:
+      errorMail()
+      logging.info(" "  + datetime.now().strftime('%Y.%m.%d %H:%M:%S') + " Nem találom a filet ")
+      exit(1)   
 
+   dateCompare = datetime.today() + timedelta(days=40)
+ 
+   for index, row in df.iterrows():
+      if row['Dátum'] < dateCompare:
+         datum = row['Dátum'].strftime("%Y.%m.%d")
+         okmany = row['Okmány']
+         sendMail(datum, okmany)
 
-def sendMail():
+def sendMail(datum, okmany):
+   sender = 'ertesito@lazarteam.hu'
+   receivers = ['kassai.dora@lazarteam.hu, konyveles@lazarteam.hu']
+
+   message = MIMEText(f'Emlékeztető email lejáró okmányról.\n {datum} {okmany}')
+
+   message['From'] = 'ertesito@lazarteam.hu'
+   message['To'] = 'kassai.dora@lazarteam.hu, konyveles@lazarteam.hu'
+   message['Subject'] = f'{okmany} Lejáró okmány {datum}'
+
+   try:
+      smtpObj = smtplib.SMTP('192.168.103.100')
+      smtpObj.sendmail(sender, receivers, message.as_string())
+   except smtplib.SMTPException as e:
+      logging.info(" " + datetime.now().strftime('%Y.%m.%d %H:%M:%S') + " Nem sikerült elküldeni a levelet ", e)
+
+def errorMail():
    sender = 'ertesito@lazarteam.hu'
    receivers = ['f.ferenc@lazarteam.hu']
 
-   message = """From: Havi ertesito <ertesito@lazarteam.hu>
-   To: To konyveles <f.ferenc@lazarteam.hu>
-   Subject: Havi emlekeztete email
+   message = MIMEText('Nem található az excel: //192.168.103.101/common/Doks/emlekezteto/emlekezteto.xlsx')
 
-   This is a test e-mail message.
-   """
+   message['From'] = 'ertesito@lazarteam.hu'
+   message['To'] = 'f.ferenc@lazarteam.hu'
+   message['Subject'] = 'Nem található az excel'
 
    try:
-      smtpObj = smtplib.SMTP('localhost')
-      smtpObj.sendmail(sender, receivers, message)
-      print("Successfully sent email")
+      smtpObj = smtplib.SMTP('192.168.103.100')
+      smtpObj.sendmail(sender, receivers, message.as_string())
    except smtplib.SMTPException:
-      print("Error: unable to send email")
+      logging.info(" " + datetime.now().strftime('%Y.%m.%d %H:%M:%S') + " Nem sikerült elküldeni a levelet ")
 
 
 
